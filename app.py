b2e684d5b06624f7d7bef91cf695363a4b025d8f
sunweb.pavithran@gmail.com <sunweb.pavithran@gmail.com>
2019-08-20 08:43:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import socketio
+from bocadillo import App, configure, Templates, static
+
+app = App()
+configure(app)
+templates = Templates()
+
+sio = socketio.AsyncServer(async_mode="asgi")
+app.mount("/sio", socketio.ASGIApp(sio))
+app.mount("/socket.io", static("node_modules/socket.io-client/dist"))
+
+@sio.on("message")
+async def broadcast(sid, data: str):
+    print("message: ", data)
+    await sio.emit("response", "data")
+
+@app.route("/")
+async def index(req, res):
+    res.html = await templates.render("index.html")
